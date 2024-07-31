94b974b7a9d3288dba07908b9bfca647f7f25cc5
mohan <mohan14242@gmail.com>
2024-07-31 17:10:37 +0530

    mohan

--- python.sh
+++ python.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+set password $env(sudo_password)
+spawn sudo apt-get install -y python3.8 python3.8-venv python3.8-dev
+expect "password for" 
+send "$password\r"
+spawn python3.8 --version
+expect eof
+
+
